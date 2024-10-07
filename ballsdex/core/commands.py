bcc59ce0e762ed43848c71f1b5fb65abf778aab9
anonwubbabunga <112557613+wubbubunga-tll@users.noreply.github.com>
2024-10-07 03:26:54 +0100

    Update commands.py

--- ballsdex/core/commands.py
+++ ballsdex/core/commands.py
@@ -2,6 +2,8 @@ import logging
 import time
 from typing import TYPE_CHECKING
 
+import discord
+from discord import app_commands
 from discord.ext import commands
 from tortoise import Tortoise
 
@@ -13,34 +15,28 @@ if TYPE_CHECKING:
 
 class Core(commands.Cog):
     """
-    Core commands of BallsDex bot
+    Core commands of BallsDexBot
     """
 
     def __init__(self, bot: "BallsDexBot"):
         self.bot = bot
 
-    @commands.command()
-    async def ping(self, ctx: commands.Context):
-        """
-        Ping!
-        """
-        await ctx.send("Pong")
+    @app_commands.command(name="ping")
+    async def ping(self, interaction: discord.Interaction):
+        """Ping!"""
+        await interaction.response.send_message("Pong")
 
-    @commands.command()
+    @app_commands.command(name="reloadtree")
     @commands.is_owner()
-    async def reloadtree(self, ctx: commands.Context):
-        """
-        Sync the application commands with Discord
-        """
+    async def reloadtree(self, interaction: discord.Interaction):
+        """Sync the application commands with Discord"""
         await self.bot.tree.sync()
-        await ctx.send("Application commands tree reloaded.")
+        await interaction.response.send_message("Application commands tree reloaded.")
 
-    @commands.command()
+    @app_commands.command(name="reload")
     @commands.is_owner()
-    async def reload(self, ctx: commands.Context, package: str):
-        """
-        Reload an extension
-        """
+    async def reload(self, interaction: discord.Interaction, package: str):
+        """Reload an extension"""
         package = "ballsdex.packages." + package
         try:
             try:
@@ -48,16 +44,16 @@ class Core(commands.Cog):
             except commands.ExtensionNotLoaded:
                 await self.bot.load_extension(package)
         except commands.ExtensionNotFound:
-            await ctx.send("Extension not found")
+            await interaction.response.send_message("Extension not found")
         except Exception:
-            await ctx.send("Failed to reload extension.")
+            await interaction.response.send_message("Failed to reload extension.")
             log.error(f"Failed to reload extension {package}", exc_info=True)
         else:
-            await ctx.send("Extension reloaded.")
+            await interaction.response.send_message("Extension reloaded.")
 
-    @commands.command()
+    @app_commands.command(name="reloadcache")
     @commands.is_owner()
-    async def reloadcache(self, ctx: commands.Context):
+    async def reloadcache(self, interaction: discord.Interaction):
         """
         Reload the cache of database models.
 
@@ -65,11 +61,11 @@ class Core(commands.Cog):
         next start.
         """
         await self.bot.load_cache()
-        await ctx.message.add_reaction("✅")
+        await interaction.response.send_message("✅ Cache reloaded")
 
-    @commands.command()
+    @app_commands.command(name="analyzedb")
     @commands.is_owner()
-    async def analyzedb(self, ctx: commands.Context):
+    async def analyzedb(self, interaction: discord.Interaction):
         """
         Analyze the database. This refreshes the counts displayed by the `/about` command.
         """
@@ -77,4 +73,4 @@ class Core(commands.Cog):
         t1 = time.time()
         await connection.execute_query("ANALYZE")
         t2 = time.time()
-        await ctx.send(f"Analyzed database in {round((t2 - t1) * 1000)}ms.")
+        await interaction.response.send_message(f"Analyzed database in {round((t2 - t1) * 1000)}ms.")
