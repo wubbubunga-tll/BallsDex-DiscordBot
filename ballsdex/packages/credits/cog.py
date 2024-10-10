2ece1e17549e264fb8ac573104e13b4b0f38ab59
Venus <hallowthehawlo@gmail.com>
2024-10-10 17:55:05 +0100

    updated creduts

--- ballsdex/packages/credits/cog.py
+++ ballsdex/packages/credits/cog.py
@@ -7,6 +7,7 @@ class Credits(commands.Cog):
         self.bot = bot
 
     @app_commands.command()
+    @app_commands.checks.cooldown(1, 10) 
     async def credits(self, interaction: discord.Interaction):
         """Display the credits for FanmadeDex"""
         try:
@@ -14,28 +15,7 @@ class Credits(commands.Cog):
             message = await channel.fetch_message(1293965631137382410)
             content = message.content
         except (discord.NotFound, discord.Forbidden, AttributeError):
-            content = """**CREDITS  🏆**
-This channel is to show credits towards people who helped the bot become what it is now!
-**GHOSTYMPA <:GHOSTYMPA:1266784573317058639>
-DDOMSM <:DDOMSM:1267481517198807060>
-Raw Zebra <:RawZebra:1269788571284410470>
-JakeTheDrake <:JakeTheDrake:1269785238398308434>
-Charlilee <:Charlilee:1273687213426737184>
-Interdimensional Music <:InterdimensionalMusic:1269794316147228703>
-Pyro<:PyroMSM:1269775583165022369>
-Slimer <:SlimerMSM:1269774390833188905>
-Nova / MSM <:NovaMSM:1267481416330121216>
-PlushyPlushMSM <:PlushyPlushMSM:1269780280109957223>
-Logan Peters <:LoganPetersMSM:1269778782076534864>
-Venus <:Venus:1293959996945469510> 
-Uksus <:Uksus:1273689692793208904>
-Astroshock25 <:Astroshock25:1275579109237587989>
-Licoad666 <:Licoad666:1275802191109820550>
-Wubtopia <:Wubtopia:1278832063616909394>
-Gorm / Demochees <:Demochees:1278832427313270848>
-Calpamos <:Calpamos:1293964105539129345>
-**
-And special thanks to **Charlilee **<:Charlilee:1273687213426737184> for creating the FanmadeDex logo art, and **Bylogic**<:Bylogic:1275813304220123247> for setting up the official server."""
+            content = """Credits failed to load, please contact support if this error continues"""
 
         embed = discord.Embed(
             title="",
